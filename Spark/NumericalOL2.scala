5466ce50d52537cd2c43949d89701c816ea24f9b
antarctica <ADMMOL2@users.noreply.github.com>
2017-05-11 14:18:29 +0800

    Update NumericalOL2.scala

--- Spark/NumericalOL2.scala
+++ Spark/NumericalOL2.scala
@@ -113,10 +113,6 @@ object NumericalOL2 {
 
     * alpha is the over-relaxation parameter (typical values for alpha are
     * between 1.0 and 1.8).
-
-    * More information can be found in the paper linked at:
-    * http://www.stanford.edu/~boyd/papers/distr_opt_stat_learning_admm.html
-
     */
   def Ridge (A:BDM[Double],b:BDV[Double],lambda:BDV[Double],rho:Double,alpha:Double): Map[String, Array[Double]] =
   {
