b9c1eb61470d023c64dd7daa13d27c1ac75fea60
antarctica <ADMMOL2@users.noreply.github.com>
2018-10-06 08:49:14 +0800

    Add files via upload

--- Figures/AVerageMSE.m
+++ Figures/AVerageMSE.m
@@ -3,17 +3,18 @@ f = figure('Color', 'white');
 %clf; 
 %L1data = load('data/L1-lasso_q.txt');
 %OL1data = load('data/OL1-q.txt');
+%L2data = load('data/L2_q.txt');
 %OL2data = load('data/OL2-q.txt');
 L1data = load('data/peLasso_q.txt');
 OL1data = load('data/peOL1_q.txt');
-OL2data = load('data/peOL2_q.txt');
+OL12data = load('data/peOL12_q.txt');
 
 x = linspace(0,1,10);
 plot(x,L1data,'r-.','MarkerSize', 10, 'LineWidth', 3);
 hold on
 plot(x,OL1data,'b-','MarkerSize', 10, 'LineWidth', 3);
 hold on
-plot(x,OL2data,'m--','MarkerSize', 10, 'LineWidth', 3);
+plot(x,OL12data,'m--','MarkerSize', 10, 'LineWidth', 3);
 hold off;
 xlim([0.1,1.0]);
 set(gca,'XTick',(0:0.1:10));
@@ -21,7 +22,7 @@ set(gca,'YScale','log');
 xlabel('q','Fontsize',18); 
 ylabel('Estimated MSE','Fontsize',18);
 % Create axes
-legend1 = legend('Lasso','OL1','OL2','Orientation','horizontal');
+legend1 = legend('Lasso','OL1','OL12','Orientation','horizontal');
 set(legend1,'FontSize',18,'FontName','Times New Roman');
 set(gca,'FontSize',18);
 
