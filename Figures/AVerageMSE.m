8d627809faf6383c3fe7e1aa972d5163499fd64c
antarctica <ADMMOL2@users.noreply.github.com>
2019-07-01 14:58:40 +0800

    Add files via upload

--- Figures/AVerageMSE.m
+++ Figures/AVerageMSE.m
@@ -7,26 +7,31 @@ f = figure('Color', 'white');
 %OL2data = load('data/OL2-q.txt');
 L1data = load('data/peLasso_q.txt');
 OL1data = load('data/peOL1_q.txt');
+OL2data = load('data/peOL2_q.txt');
 OL12data = load('data/peOL12_q.txt');
 
-x = linspace(0,1,10);
+
+x = linspace(0,1,5);
 plot(x,L1data,'r-.','MarkerSize', 10, 'LineWidth', 3);
 hold on
 plot(x,OL1data,'b-','MarkerSize', 10, 'LineWidth', 3);
 hold on
+plot(x,OL2data,'g--','MarkerSize', 10, 'LineWidth', 3);
+hold on
 plot(x,OL12data,'m--','MarkerSize', 10, 'LineWidth', 3);
 hold off;
-xlim([0.1,1.0]);
-set(gca,'XTick',(0:0.1:10));
+xlim([0.1,0.5]);
+set(gca,'XTick',(0:0.1:5));
 set(gca,'YScale','log');
 xlabel('q','Fontsize',18); 
 ylabel('Estimated MSE','Fontsize',18);
 % Create axes
-legend1 = legend('Lasso','OL1','OL12','Orientation','horizontal');
-set(legend1,'FontSize',18,'FontName','Times New Roman');
+
+legend1 = legend('Lasso','$$O\ell_{1}$$','$$O\ell_{2}$$','$$O\ell_{1,2}$$','Orientation','horizontal');
+set(legend1,'FontSize',18,'FontName','Times New Roman','Interpreter','latex');
 set(gca,'FontSize',18);
 
 %title('q = 0.4');
 %print('E:\Deep Learning\Research\Paper Writting\graphs\AvgMse_q.png','-dpng','-r900');
-%print('E:\Deep Learning\Research\Paper Writting\graphs\AvgMse_q.eps','-depsc2','-r900');
+%print('E:\Deep Learning\Research\Paper Writting\paper\ICBK 2019\Fig7.eps','-depsc2','-r900');
 
